30d888ea56aab514e6eed1961db9327da5e36c25
Matt Kauper <mhk4g@virginia.edu>
2017-07-25 11:17:05 -0400

    Make VisualLogger a framework

---
+++ HyperionCore/VisualLogger/VisualLogger.h
@@ -0,0 +1,19 @@
+//
+//  VisualLogger.h
+//  VisualLogger
+//
+//  Created by Matt Kauper on 7/25/17.
+//  Copyright © 2017 WillowTree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VisualLogger.
+FOUNDATION_EXPORT double VisualLoggerVersionNumber;
+
+//! Project version string for VisualLogger.
+FOUNDATION_EXPORT const unsigned char VisualLoggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VisualLogger/PublicHeader.h>
+
+
--- build.sh
+++ build.sh
@@ -5,3 +5,4 @@ mkdir output
 sh build-umbrella-framework.sh HyperionCore
 sh build-umbrella-framework.sh SlowAnimations
 sh build-umbrella-framework.sh Impeccable
+sh build-umbrella-framework.sh VisualLogger
