23bb4d6e0a6090ca24577fe5be2370ccecb8252b
Ben Humphries <imachumphries@me.com>
2019-05-09 15:47:03 -0400

    Add script to generate jazzy docs

---
+++ generate-docs.sh
@@ -0,0 +1,10 @@
+bundle exec jazzy \
+--objc \
+--clean \
+--author WillowTree \
+--author_url https://willowtreeapps.com \
+--github_url https://github.com/willowtreeapps/Hyperion-iOS \
+--output docs \
+--umbrella-header Core/HyperionCore.h \
+--framework-root . \
+--sdk iphone
